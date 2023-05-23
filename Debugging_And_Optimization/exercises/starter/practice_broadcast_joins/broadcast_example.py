b3c20c6cc56b515e04088327c57a6e62d39d5d4a
Robert West <robert.david.west@gmail.com>
2023-05-23 11:36:30 -0400

    Changed variable name

--- Debugging_And_Optimization/exercises/starter/practice_broadcast_joins/broadcast_example.py
+++ Debugging_And_Optimization/exercises/starter/practice_broadcast_joins/broadcast_example.py
@@ -4,13 +4,13 @@ from pyspark import SparkContext
 
 sc = SparkContext('local[*]', 'pyspark')
 
-my_dict = {"item1": 1, "item2": 2, "item3": 3, "item4": 4} 
+my_dict = {"item1": 1, "item2": 2, "item3": 3, "item4": 4}
 my_list = ["item1", "item2", "item3", "item4"]
 
 my_dict_bc = sc.broadcast(my_dict)
 
-def my_func(letter):
-    return my_dict_bc.value[letter] 
+def my_func(item):
+    return my_dict_bc.value[item]
 
 my_list_rdd = sc.parallelize(my_list)
 
